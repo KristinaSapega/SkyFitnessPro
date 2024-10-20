12f5c6af63824faf3559c3fcd264c5228e3ecfcb
dimych1978 <dimych1978@gmail.com>
2024-10-20 18:30:59 +0300

    create login and password modules

--- my-project/src/components/Main.tsx
+++ my-project/src/components/Main.tsx
@@ -1,25 +1,21 @@
-import TrainingList from "./trainingList";
+import TrainingList from "./TrainingList";
 
 const Main = () => {
   return (
-    <main className="pt-[60px]">
-    <div className="relative">
-      <h1 className="w-[850px] h-[120px] font-medium text-6xl ">
-        Начните заниматься спортом и улучшите качество жизни
-      </h1>
-      <img
-        className="absolute right-0 top-0"
-        src="cloudImage.png"
-        alt="text"
-      />
-    </div>
-    <TrainingList />
-    <div className="flex justify-center mt-[34px] mb-[81px]">
-      <button className="w-[127px] h-[52px] rounded-[46px] bg-[#BCEC30]">
-        Наверх ↑
-      </button>
-    </div>
-  </main>
+    <main className="w-full p-16 pt-[60px]">
+      <header className="flex justify-between">
+        <h1 className="h-[120px] w-[850px] text-6xl font-medium">
+          Начните заниматься спортом и улучшите качество жизни
+        </h1>
+        <img className="" src="cloudImage.png" alt="text" />
+      </header>
+      <TrainingList />
+      <div className="mb-[81px] mt-[34px] flex justify-center">
+        <button className="h-[52px] w-[127px] rounded-[46px] bg-[#BCEC30]">
+          Наверх ↑
+        </button>
+      </div>
+    </main>
   );
 };
 
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["Roboto"],
+      },
+      colors(theme) {
+        return {
+          btnPrimaryActive: "#000",
+          btnPrimaryHover: " #C6FF00",
+          btnPrimaryRegular: "#BCEC30",
+          btnPrimaryInactive: "#F7F7F7",
+          btnSecondaryActive: "#E9ECED",
+          btnSecondaryHover: " #F7F7F7",
+          btnSecondaryRegular: "#fff",
+          btnSecondaryInactive: "#fff",
+        };
+      },
+      animation: {
+        err: "pulse 20ms 20",
+      },
+    },
   },
   plugins: [],
-}
-
+};
