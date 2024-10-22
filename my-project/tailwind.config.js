d23dbbca6f31ea815f1ac5fee4b02fa77a84dfc0
ASaukov <Saukov72@mail.ru>
2024-10-23 01:45:03 +0500

    addition tailwind

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,19 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: {
-      'tablet': '375px',
-      'laptop': '768px',
-      'desktop': '1024px'
-        },
+      fontFamily: {
+        sans: ["Roboto"],
       },
+      colors(theme) {
+        return {
+          btnPrimaryActive: "#000",
+          btnPrimaryHover: " #C6FF00",
+          btnPrimaryRegular: "#BCEC30",
+          btnPrimaryInactive: "#F7F7F7",
+          btnSecondaryActive: "#E9ECED",
+          btnSecondaryHover: " #F7F7F7",
+          btnSecondaryRegular: "#fff",
+          btnSecondaryInactive: "#fff",
+        };
+      },
+      animation: {
+        err: "pulse 20ms 20",
+      },
+      screens: {
+          'tablet': '375px',
+          'laptop': '768px',
+          'desktop': '1024px',
+            },
+    },
   },
-  
   plugins: [],
-}
-
+};
