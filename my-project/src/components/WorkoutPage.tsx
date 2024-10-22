87e903705e465d15a150e5742c66e10982935b8d
ASaukov <Saukov72@mail.ru>
2024-10-23 00:42:49 +0500

    adaptive for mobile

--- my-project/src/components/WorkoutPage.tsx
+++ my-project/src/components/WorkoutPage.tsx
@@ -1,49 +1,49 @@
 export const WorkoutPage = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-[1440px] px-[140px]">
-        <header className="h-[145px] flex justify-between items-center">
+      <div className="w-[375px] px-[16px] desktop:w-[1440px] desktop:px-[140px]">
+        <header className="h-[115px] desktop:h-[145px] flex justify-between items-center">
           <div className="">
             <a href="">
               <img src="logo.svg" alt="logo" />
             </a>
           </div>
-          <nav className="flex items-center gap-[20px]">
+          <nav className="flex items-center gap-[13px] desktop:gap-[16px]">
             <img className="" src="avatar_mini.svg" alt="avatar" />
-            <a className="flex gap-2 items-center" href="">
-              <h3 className="text-2xl">Сергей</h3>
-              <div className="size-2 border-b-2 border-l-2 border-black rotate-[-45deg]"></div>
+            <a className="flex gap-3 items-center" href="">
+              <h3 className="hidden desktop:text-2xl desktop:block">Сергей</h3>
+              <div className="size-1.5 desktop:size-2 border-b-2 border-l-2 border-black rotate-[-45deg]"></div>
             </a>
           </nav>
         </header>
         <main className="">
           <section className="">
-            <h1 className="text-6xl font-medium mb-6">Йога</h1>
-            <h2 className="text-[32px] underline underline-offset-6">
+            <h1 className="text-[24px] desktop:text-[60px] font-medium mb-[10px] desktop:mb-6">Йога</h1>
+            <h2 className="text-[18px] desktop:text-[32px] underline underline-offset-6">
               Красота и здоровье / Йога на каждый день / 2 день
             </h2>
             <iframe
-              className="my-[40px] w-full aspect-video h-auto max-w-full rounded-3xl"
+              className="my-6 desktop:my-[40px] w-full aspect-video h-auto max-w-full rounded-[8.87px] desktop:rounded-3xl"
               src="https://www.youtube.com/embed/v-xTLFDhoD0"
             ></iframe>
           </section>
-          <div className="mb-[200px] rounded-3xl shadow-[0px_4px_67px_-12px_#00000021]">
-            <section className="p-[40px]">
+          <div className="mb-[84px] desktop:mb-[200px] rounded-[30px] desktop:rounded-3xl shadow-[0px_4px_67px_-12px_#00000021]">
+            <section className="p-[30px] desktop:p-[40px]">
               <h2 className="text-[32px]">Упражнения тренировки 2</h2>
-              <ul className="grid grid-cols-3  gap-x-[60px]">
+              <ul className="pt-5 flex flex-col gap-6 desktop:grid grid-cols-3 desktop:gap-x-[60px]">
                 <li className=" flex flex-col">
-                  <label className="text-lg pb-[10px]" htmlFor="">
-                    Название упражнения %
+                  <label className="text-[18px] desktop:text-lg pb-[10px]" htmlFor="">
+                    Упражнение %
                   </label>
                   <progress
-                    className="w-80 h-[6px] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-[#F7F7F7] [&::-webkit-progress-value]:bg-[#00C1FF] [&::-moz-progress-bar]:bg-[#00C1FF]"
+                    className="w-[283px] desktop:w-80 h-[6px] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-[#F7F7F7] [&::-webkit-progress-value]:bg-[#00C1FF] [&::-moz-progress-bar]:bg-[#00C1FF]"
                     id="progress"
                     value="15"
                     max="100"
                   ></progress>
                 </li>
               </ul>
-              <button className="mt-10 bg-[#BCEC30] hover:bg-[#C6FF00] active:bg-black active:text-white w-80 h-[52px] rounded-full text-lg">
+              <button className="mt-10 bg-[#BCEC30] hover:bg-[#C6FF00] active:bg-black active:text-white w-[283px] desktop:w-80 h-[52px] rounded-full text-lg">
                 {/* Если прогесс заполнен то текст на кнопке используем `Обновить свой прогресс` */}
                 Заполнить свой прогресс
               </button>
@@ -54,3 +54,13 @@ export const WorkoutPage = () => {
     </div>
   );
 };
+
+// Для tailwind.config
+
+// extend: {
+//   screens: {
+//   'tablet': '375px',
+//   'laptop': '768px',
+//   'desktop': '1024px'
+//     },
+//   },
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+      'tablet': '375px',
+      'laptop': '768px',
+      'desktop': '1024px'
+        },
+      },
   },
+  
   plugins: [],
 }
 
