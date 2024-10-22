3171ba822f9cddb0daac7f2ec38164bf0cc1548f
ASaukov <Saukov72@mail.ru>
2024-10-23 02:42:42 +0500

    size desktop

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -24,7 +24,7 @@ export default {
       screens: {
           'tablet': '375px',
           'laptop': '768px',
-          'desktop': '1024px',
+          'desktop': '1440px',
             },
     },
   },
