466b2461c1638648a77043436787e1de3f451d44
ASaukov <Saukov72@mail.ru>
2024-10-26 23:49:01 +0500

    part corrected

--- my-project/src/components/WorkoutPage.tsx
+++ my-project/src/components/WorkoutPage.tsx
@@ -1,50 +1,67 @@
+import { json } from "react-router-dom";
+import Header from "./Header";
+
+const workout = {
+  _id: "hfgxlo",
+  exercises: [
+    {
+      name: "Наклон вперед",
+      quantity: 10,
+    },
+    {
+      name: "Наклон назад",
+      quantity: 10,
+    },
+    {
+      name: "Поднятие ног, согнутых в коленях",
+      quantity: 5,
+    },
+  ],
+  name: "Красота и здоровье / Йога на каждый день / 2 день / Алексей Казубский",
+  video: "https://www.youtube.com/embed/v-xTLFDhoD0",
+};
+
+
 export const WorkoutPage = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="w-[375px] px-[16px] desktop:w-[1440px] desktop:px-[140px]">
-        <header className="h-[115px] desktop:h-[145px] flex justify-between items-center">
-          <div className="">
-            <a href="">
-              <img src="logo.svg" alt="logo" />
-            </a>
-          </div>
-          <nav className="flex items-center gap-[13px] desktop:gap-[16px]">
-            
-            <a className="flex gap-3 items-center" href="">
-            <img className="w-[30px] desktop:w-10" src="avatar_mini.svg" alt="avatar" />
-              <h3 className="hidden desktop:text-2xl desktop:block">Сергей</h3>
-              <div className="size-1.5 desktop:size-2 border-b-2 border-l-2 border-black rotate-[-45deg]"></div>
-            </a>
-          </nav>
-        </header>
+        <Header />
         <main className="">
           <section className="">
-            <h1 className="text-[24px] desktop:text-[60px] font-medium mb-[10px] desktop:mb-6">Йога</h1>
-            <h2 className="text-[18px] desktop:text-[32px] underline underline-offset-6">
-              Красота и здоровье / Йога на каждый день / 2 день
-            </h2>
+            <h1 className="mb-[10px] text-[24px] font-medium desktop:mb-6 desktop:text-[60px]">
+              Йога
+            </h1>
+              <span className="text-[18px] underline desktop:text-[32px] ">
+              {workout.name}
+              </span>
             <iframe
-              className="my-6 desktop:my-[40px] w-full aspect-video h-auto max-w-full rounded-[8.87px] desktop:rounded-3xl"
-              src="https://www.youtube.com/embed/v-xTLFDhoD0"
+              className="my-6 aspect-video h-auto w-full max-w-full rounded-[8.87px] desktop:my-[40px] desktop:rounded-3xl"
+              src={workout.video}
             ></iframe>
           </section>
-          <div className="mb-[84px] desktop:mb-[200px] rounded-[30px] desktop:rounded-3xl shadow-[0px_4px_67px_-12px_#00000021]">
+          <div className="mb-[84px] rounded-[30px] shadow-[0px_4px_67px_-12px_#00000021] desktop:mb-[200px] desktop:rounded-3xl">
             <section className="p-[30px] desktop:p-[40px]">
               <h2 className="text-[32px]">Упражнения тренировки 2</h2>
-              <ul className="pt-5 flex flex-col gap-6 desktop:grid grid-cols-3 desktop:gap-x-[60px]">
-                <li className=" flex flex-col">
-                  <label className="text-[18px] desktop:text-lg pb-[10px]" htmlFor="">
-                    Название упражнения %
-                  </label>
-                  <progress
-                    className="w-[283px] desktop:w-80 h-[6px] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-bar]:bg-[#F7F7F7] [&::-webkit-progress-value]:bg-[#00C1FF] [&::-moz-progress-bar]:bg-[#00C1FF]"
-                    id="progress"
-                    value="15"
-                    max="100"
-                  ></progress>
-                </li>
+              <ul className="flex grid-cols-3 flex-col gap-6 pt-5 desktop:grid desktop:gap-x-[60px]">
+                {workout.exercises.map((items) => (
+                  <li className="flex flex-col">
+                    <label
+                      className="pb-[10px] text-[18px] desktop:text-lg"
+                      htmlFor=""
+                    >
+                      {items.name} {100 / items.quantity }%
+                    </label>
+                    <progress
+                      className="h-[6px] w-[283px] desktop:w-80 [&::-moz-progress-bar]:bg-[#00C1FF] [&::-webkit-progress-bar]:rounded-3xl [&::-webkit-progress-bar]:bg-[#F7F7F7] [&::-webkit-progress-value]:rounded-3xl [&::-webkit-progress-value]:bg-[#00C1FF]"
+                      id="progress"
+                      value="5"
+                      max={items.quantity}
+                    ></progress>
+                  </li>
+                ))}
               </ul>
-              <button className="mt-10 bg-[#BCEC30] hover:bg-[#C6FF00] active:bg-black active:text-white w-[283px] desktop:w-80 h-[52px] rounded-full text-lg">
+              <button onClick={()=>(console.log("Открывается модалка с выбором тренировки"))} className="mt-10 h-[52px] w-[283px] rounded-full bg-[#BCEC30] text-lg hover:bg-[#C6FF00] active:bg-black active:text-white desktop:w-80">
                 {/* Если прогесс заполнен то текст на кнопке используем `Обновить свой прогресс` */}
                 Заполнить свой прогресс
               </button>
@@ -56,12 +73,3 @@ export const WorkoutPage = () => {
   );
 };
 
-// Для tailwind.config
-
-// extend: {
-//   screens: {
-//   'tablet': '375px',
-//   'laptop': '768px',
-//   'desktop': '1024px'
-//     },
-//   },
---
+++ my-project/src/components/WorkoutsList.tsx
@@ -0,0 +1,27 @@
+// type WorkoutType = {
+//     id: string,
+//     exercises: [string, number | null],
+//     name: string,
+//     quantity: number | null,
+// }
+
+export const workout = 
+{
+    _id: "hfgxlo",
+    exercises: [
+      {
+        name: "Наклон вперед",
+        quantity: 10
+      },
+      {
+        name: "Наклон назад",
+        quantity: 10
+      },
+      {
+        name: "Поднятие ног, согнутых в коленях",
+        quantity: 5
+      }
+    ],
+    name: "Красота и здоровье / Йога на каждый день / 2 день / Алексей Казубский",
+    video: "https://www.youtube.com/embed/v-xTLFDhoD0"
+  };
