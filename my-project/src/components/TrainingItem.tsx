58da5c7f72dcbb2885e39d558958851ed6743241
Fantom52r <fantom52rs@gmail.com>
2024-10-27 18:17:16 +0300

    BUZZ_FIX

--- my-project/src/components/TrainingItem.tsx
+++ my-project/src/components/TrainingItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Tags from "./Tags";
+import { items } from "../data";
 
 interface Train {
   id: number;
@@ -12,19 +13,19 @@ interface Train {
 
 const TrainingItem: React.FC<{ train: Train }> = ({ train }) => {
   return (
-    <li className="w-[360px] h-[510px] rounded-[30px] bg-[white] shadow-[0px_4px_67px_-12px_#00000021] relative">
-      <div className="overflow-hidden h-[325px] rounded-[30px]">
+    <li className="relative h-[510px] w-[360px] rounded-[30px] bg-[white] shadow-[0px_4px_67px_-12px_#00000021]">
+      <div className="h-[325px] overflow-hidden rounded-[30px]">
         <img
           className="w-full object-cover"
           src={train.urlImg}
           alt={train.trainType}
         />
       </div>
-      <div className="py-[24px] px-[30px]">
-        <h3 className="font-medium text-3xl mb-[20px]">{train.trainType}</h3>
+      <div className="px-[30px] py-[24px]">
+        <h3 className="mb-[20px] text-3xl font-medium">{train.trainType}</h3>
         <ul className="flex flex-wrap gap-[6px]">
           {[train.calendar, train.time, train.level].map((tag, index) => (
-            <Tags tag={tag} index={index}/>
+            <Tags tag={tag} index={index} key={index} />
           ))}
         </ul>
       </div>
--- my-project/src/components/TrainingList.tsx
+++ my-project/src/components/TrainingList.tsx
@@ -1,52 +1,7 @@
 import React from "react";
 import TrainingItem from "./TrainingItem";
 import { useNavigate } from "react-router-dom";
-const items = [
-  {
-    id: 1,
-    urlImg: "yoga.png",
-    trainType: "йога",
-    calendar: "25 дней",
-    time: "25-50 мин/день",
-    level: "Сложность",
-  },
-
-  {
-    id: 2,
-    urlImg: "stretching.png",
-    trainType: "Стретчинг",
-    calendar: "25 дней",
-    time: "25-50 мин/день",
-    level: "Сложность",
-  },
-
-  {
-    id: 3,
-    urlImg: "danceFitness.png",
-    trainType: "Зумба",
-    calendar: "25 дней",
-    time: "25-50 мин/день",
-    level: "Сложность",
-  },
-
-  {
-    id: 4,
-    urlImg: "stepAirobic.png",
-    trainType: "Степ-аэробика",
-    calendar: "25 дней",
-    time: "25-50 мин/день",
-    level: "Сложность",
-  },
-
-  {
-    id: 5,
-    urlImg: "bodyFlex.png",
-    trainType: "Бодифлекс",
-    calendar: "25 дней",
-    time: "25-50 мин/день",
-    level: "Сложность",
-  },
-];
+import { items } from "../data";
 
 const TrainingList = () => {
   const navigate = useNavigate();
