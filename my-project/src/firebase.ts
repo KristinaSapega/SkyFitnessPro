ca7bb7db11e9525f4f3f13ae4596bffe81a81291
Евгений Свиридов <svirid.evg@yandex.ru>
2024-10-30 00:58:35 +0300

    6.2 Get Data + Readme

--- my-project/src/firebase.ts
+++ my-project/src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZwFyvnV5MTOo45tEXxcEo85MLH97_mHg",
@@ -14,29 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-export { auth };
-
-// Инструкция по использованию 
-// 1. npm install
-
-// 2. Импортируйте и вызовите signInWithEmailAndPassword(auth, email, pass) - Авторизация
-
-// 3. Импортируйте и вызовите createUserWithEmailAndPassword(auth, email, pass) - Регистрация
-
-// 4. Импортируйте и вызовите signOut(auth) - Выход из учетной записи
-
-// 5. Импортируйте и вызовите sendPasswordResetEmail(auth, email) - восстановление пароля
-
-// 6. Импортируйте и вызовите onAuthStateChanged(auth, (user) =>{}) - следить за состоянием пользователя, при наличии user запишите в состояние его данные"
-
-// 7. Импортируйте и вызовите updateProfile(currentUser, { displayName: "Jane Q. User", photoURL: "https://example.com/jane-q-user/profile.jpg", }) - для изменения данных пользователя Имя и Фото
-
-// 8. Обновление пароля 
-// const user = auth.currentUser;
-// const newPassword = "данные из импута";
-// Импортируйте и вызовите updatePassword(user, newPassword)
-
-// auth - импортируется отдельно из firebase.ts
-
-// все функции возвращают ответ можно обрабатывать через then и catch  
+export { auth, database };
