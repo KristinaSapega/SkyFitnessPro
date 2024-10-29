16b8ea227b086b8e5e6d69f7d82baf566308269c
dimych1978 <dimych1978@gmail.com>
2024-10-29 06:56:00 +0300

    trash

--- my-project/src/pages/loginPage.tsx
+++ my-project/src/pages/loginPage.tsx
@@ -5,7 +5,7 @@ type IAppProps = {};
 
 export function LoginPage(props: IAppProps) {
   return (
-    <div className="entry w-full px-[140px]">
+    <div className="w-full px-[140px]">
       <Main />
       <div className="entry absolute left-0 top-0 h-full w-full min-w-[375px]">
         <Login />
