14e2b7dc4de6d7f82663e2adab220d0860cfdd8c
Евгений Свиридов <svirid.evg@yandex.ru>
2024-11-01 02:12:48 +0300

    POST_DATA method + 13 Header functional

--- my-project/src/components/HeaderPopUp.tsx
+++ my-project/src/components/HeaderPopUp.tsx
@@ -1,20 +1,45 @@
 "use client";
 
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import { User } from "firebase/auth";
+
 export const HeaderUserPopUp = () => {
+  const user: User | null = auth.currentUser;
+  const email: string | null = user?.email ?? null;
+  const name: string | null = user?.displayName ?? null;
+
+  const navigate = useNavigate();
+
+  const handleOpenUserPage = () => {
+    navigate("/user");
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
   return (
-    <div className="shadowBlack013 absolute right-[140px] top-[70px] box-border flex h-[258px] w-[266px] flex-col items-center rounded-[30px] bg-white p-[30px]">
+    <div className="shadowBlack013 box-border flex h-[258px] w-[266px] flex-col items-center rounded-[30px] bg-white p-[30px]">
       <p className="text-[18px] font-normal leading-[19.8px] text-black">
-        Сергей
+        {name}
       </p>
       <p className="mt-[10px] text-[18px] font-normal leading-[19.8px] text-[#999999]">
-        sergey.petrov96@mail.ru
+        {email}
       </p>
 
       <div className="mt-[34px] flex flex-col gap-[10px]">
-        <button className="buttonPrimary w-[206px] hover:bg-btnPrimaryHover active:bg-btnPrimaryActive disabled:bg-btnPrimaryInactive">
+        <button
+          onClick={handleOpenUserPage}
+          className="buttonPrimary w-[206px] hover:bg-btnPrimaryHover active:bg-btnPrimaryActive disabled:bg-btnPrimaryInactive"
+        >
           Мой профиль
         </button>
-        <button className="buttonSecondary w-[206px] border-[1px] border-solid border-black bg-white invalid:bg-btnSecondaryInactive hover:bg-btnSecondaryHover active:bg-btnSecondaryActive">
+        <button
+          onClick={handleLogout}
+          className="buttonSecondary w-[206px] border-[1px] border-solid border-black bg-white invalid:bg-btnSecondaryInactive hover:bg-btnSecondaryHover active:bg-btnSecondaryActive"
+        >
           Выйти
         </button>
       </div>
