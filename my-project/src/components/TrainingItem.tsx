9d070eb6dc32c7d65c41fb622ebaef8ffae354cb
Fantom52r <fantom52rs@gmail.com>
2024-11-04 21:20:47 +0300

    main paghe_login

--- my-project/src/components/TrainingItem.tsx
+++ my-project/src/components/TrainingItem.tsx
@@ -4,7 +4,9 @@ import { MainCardsImage } from "./MainCardsImage";
 import Tags from "./Tags";
 import { useNavigate } from "react-router-dom";
 import { User } from "./User";
-import { Component, useState } from "react";
+import { useModal } from "../hooks/useModal";
+import Registry from "./Registry";
+import Login from "./Login";
 
 type Component = {
   train: {
@@ -20,52 +22,56 @@ type Component = {
 };
 let user = null; //user - берем из состояния
 //список занятий пользователя
-const TrainingItem: React.FC<{ train: Component }> = ({ train,setUserTrainList }) => {
+const TrainingItem: React.FC<{ train: Component }> = ({
+  train,
+}) => {
   const currentTrain = Object.values(train)[0];
   const navigate = useNavigate();
-  const handleClickAddTrain = (e) => {
-    if (user) {
-      setUserTrainList(prevState => [...prevState, train]);
-    }
-    else {
-navigate("/login");
-e.stopPropagation()
-    }
+  const { changeValue, isRegistry } = useModal();
+
+
+
+  const handleClickAddTrain = () => {
+    alert("Пользователь не авторизован");
+    changeValue();
+
   };
 
   const handleClick = () => {
     navigate(`/course/${currentTrain._id}`);
   };
 
-  console.log(currentTrain.workouts.length);
+
+
+
 
   return (
-    <li
-      onClick={handleClick}
-      className="relative h-[510px] w-[360px] cursor-pointer rounded-[30px] bg-[white] shadow-[0px_4px_67px_-12px_#00000021]"
-    >
-      <MainCardsImage param={currentTrain._id} />
-
-      <div className="px-[30px] py-[24px]">
-        <h3 className="mb-[20px] text-3xl font-medium">
-          {currentTrain.nameRU}
-        </h3>
-        <ul className="flex flex-wrap gap-[6px]">
-          {[
-            currentTrain.workouts.length + " Дней",
-            "25-50 мин/день",
-            "Сложность",
-          ].map((tag, index) => (
-            <Tags tag={tag} index={index} key={index} />
-          ))}
-        </ul>
-      </div>
+    <li className="relative h-[510px] w-[360px] cursor-pointer rounded-[30px] bg-[white] shadow-[0px_4px_67px_-12px_#00000021]">
       <button
         onClick={handleClickAddTrain}
         className="absolute right-[20px] top-[20px]"
       >
         <img src="add-in-Circle.svg" alt="plus" />
       </button>
+      <div onClick={handleClick}>
+        <MainCardsImage param={currentTrain._id} />
+
+        <div className="px-[30px] py-[24px]">
+          <h3 className="mb-[20px] text-3xl font-medium">
+            {currentTrain.nameRU}
+          </h3>
+          <ul className="flex flex-wrap gap-[6px]">
+            {[
+              currentTrain.workouts.length + " Дней",
+              "25-50 мин/день",
+              "Сложность",
+            ].map((tag, index) => (
+              <Tags tag={tag} index={index} key={index} />
+            ))}
+          </ul>
+        </div>
+      </div>
+      {isRegistry ? <Registry /> : <Login />}
     </li>
   );
 };
--- my-project/src/components/TrainingList.tsx
+++ my-project/src/components/TrainingList.tsx
@@ -3,62 +3,8 @@ import { onValue, ref } from "firebase/database";
 import { database } from "../firebase";
 import { useEffect, useState } from "react";
 
-// const items = [
-//   {
-//     id: 1,
-//     urlImg: "yoga.png",
-//     trainType: "йога",
-//     trainName: "yoga",
-//     calendar: "25 дней",
-//     time: "25-50 мин/день",
-//     level: "Сложность",
-//   },
-
-//   {
-//     id: 2,
-//     urlImg: "stretching.png",
-//     trainType: "Стретчинг",
-//     trainName: "stretching",
-//     calendar: "25 дней",
-//     time: "25-50 мин/день",
-//     level: "Сложность",
-//   },
-
-//   {
-//     id: 3,
-//     urlImg: "danceFitness.png",
-//     trainType: "Зумба",
-//     trainName: "dance-fitness",
-//     calendar: "25 дней",
-//     time: "25-50 мин/день",
-//     level: "Сложность",
-//   },
-
-//   {
-//     id: 4,
-//     urlImg: "stepAirobic.png",
-//     trainType: "Степ-аэробика",
-//     trainName: "step-airobic",
-//     calendar: "25 дней",
-//     time: "25-50 мин/день",
-//     level: "Сложность",
-//   },
-
-//   {
-//     id: 5,
-//     urlImg: "bodyFlex.png",
-//     trainType: "Бодифлекс",
-//     trainName: "body-flex",
-//     calendar: "25 дней",
-//     time: "25-50 мин/день",
-//     level: "Сложность",
-//   },
-// ];
-
 const TrainingList = () => {
   const [items, setItems] = useState([]);
-
-  const [userTrainList, setUserTrainList] = useState([]);
 
   useEffect(() => {
     const dataRef = ref(database, "/courses");
@@ -70,7 +16,7 @@ const TrainingList = () => {
   return (
     <ul className="mt-[50px] flex flex-wrap gap-[40px]">
       {items.map((item, index) => (
-        <TrainingItem train={item} key={index} setUserTrainList={setUserTrainList} />
+        <TrainingItem train={item} key={index} />
       ))}
     </ul>
   );
