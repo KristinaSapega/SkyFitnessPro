0ccbc35298652b07fba3c3e8897fb64905fef8d0
Fantom52r <fantom52rs@gmail.com>
2024-11-11 05:46:45 +0300

    adaptive_for_all_Check

--- my-project/src/components/Main.tsx
+++ my-project/src/components/Main.tsx
@@ -7,14 +7,14 @@ import Registry from "./Registry";
 const Main = () => {
   return (
     <main className="flex flex-col items-center">
-      <div className="min-w-[375px] max-w-[1160px]  min-h-[500px]">
+      <div className="min-h-[500px] min-w-[375px] max-w-[1160px]">
         <Header />
-        <div className="mt-[40px] desktop:mt-[60px] flex justify-between sm:px-0 px-6 ">
-          <h1 className="h-[105px] max-w-[327px] text-left text-[32px] font-medium leading-[35.2px] desktop:leading-[60px] underline decoration-transparent sm:h-[120px] sm:max-w-[850px] sm:text-6xl sm:leading-normal " >
+        <div className="mt-[40px] flex justify-between px-6 sm:px-0 desktop:mt-[60px]">
+          <h1 className="h-[105px] max-w-[327px] text-left font-['Roboto'] text-[32px] font-medium leading-[35.2px] sm:h-[120px] sm:max-w-[850px] sm:text-6xl sm:leading-[normal]">
             Начните заниматься спортом и улучшите качество жизни
           </h1>
 
-          <div className="relative hidden h-[102px] max-w-[288px]  desktop:py-[16px] rounded-[5px] bg-[#BCEC30] px-[20px] text-[32px] leading-[35px] 2xl:block">
+          <div className="relative hidden h-[102px] max-w-[288px] rounded-[5px] bg-[#BCEC30] px-[20px] text-[32px] leading-[35px] desktop:py-[16px] 2xl:block">
             <p>Измени своё тело за полгода!</p>
             <img
               className="absolute bottom-[-24px] right-[140px]"
