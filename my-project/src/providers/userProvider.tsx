e289a5d38db1eec1f9045f2838aca765c93182ca
dimych1978 <dimych1978@gmail.com>
2024-11-17 09:25:31 +0300

    change userProvider.tsx

--- my-project/src/providers/userProvider.tsx
+++ my-project/src/providers/userProvider.tsx
@@ -2,23 +2,23 @@ import { createContext, ReactNode, useState } from "react";
 
 export type ModalType = {
   isOpen: boolean;
-  kindOfModal: "login" | "registry" | "info";
+  kindOfModal: "login" | "registry" | "info" | "event";
   changeOpenValue: () => void;
-  changeModal: (name: "login" | "registry" | "info") => void;
+  changeModal: (name: "login" | "registry" | "info" | "event") => void;
 };
 
 export const UserContext = createContext<ModalType | undefined>(undefined);
 
 const UserProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [kindOfModal, setKindOfModal] = useState<"login" | "registry" | "info">(
-    "login",
-  );
+  const [kindOfModal, setKindOfModal] = useState<
+    "login" | "registry" | "info" | "event"
+  >("login");
 
   const changeOpenValue = () => {
     setIsOpen(!isOpen);
   };
-  const changeModal = (name: "login" | "registry" | "info") => {
+  const changeModal = (name: "login" | "registry" | "info" | "event") => {
     setKindOfModal(name);
   };
 
